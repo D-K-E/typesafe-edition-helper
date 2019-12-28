7f387a8aaa26067e88af26cbac0b09067e97e45d
Kaan Eraslan <kaaneraslan@gmail.com>
2019-12-28 02:17:13 +0100

    UnitData implements typeclasses and maker
    UnitData implements typeclasses and maker functions.

---
+++ edition-helper/src/Control/Pure/UnitData.hs
@@ -0,0 +1,66 @@
+{-|
+Module : Model
+License : see LICENSE
+Description : UnitData control structure
+Copyright : Kaan Eraslan
+Maintainer : Kaan Eraslan
+Stability : Experimental
+-}
+module Control.Pure.ModelId
+    ( makeModelIdFromString
+    , makeModelIdFromText
+    , makeModelIdFromIdTuple
+    )
+where
+
+-- start def
+import           Primitive.Instance.Pure.UnitData
+                                                ( UnitData(..) )
+import           Primitive.Definition.Error     ( StringValueError(..)
+                                                , IdTupleValueError(..)
+                                                )
+import           FunctionDef.Setter             ( StringLike2Primitive
+                                                    ( fromString
+                                                    , fromText
+                                                    )
+                                                , TupleString2Primitive
+                                                    ( fromTupleString
+                                                    )
+                                                )
+-- end def
+import           Utils.StrUtils                 ( isAlphaNumStr
+                                                , isAsciiStr
+                                                )
+import           Data.Text                      ( Text
+                                                , pack
+                                                , unpack
+                                                ) -- importing type
+
+-- start maker
+
+-- |'makeUnitDataFromString' makes model id from string using conditions
+makeUnitDataFromString :: String -> Either StringValueError UnitData
+makeUnitDataFromString astr | null astr = Left (EmptyStr "UnitData")
+                            | otherwise = fromString astr
+
+-- |'makeUnitDataFromText' makes model id from text using conditions
+makeUnitDataFromText :: Text -> Either StringValueError UnitData
+makeUnitDataFromText txt = makeUnitDataFromString (unpack txt)
+
+-- |'makeUnitDataFromIdTuple' make model id from id tuple
+makeUnitDataFromIdTuple :: (String, String) -> Either IdTupleValueError UnitData
+makeUnitDataFromIdTuple (str1, str2)
+    | null str1
+    = Left (FirstValueError (EmptyStr "IdTuple first argument"))
+    | str1 != "udata"
+    = Left
+        (FirstValueError
+            (OtherStringError "IdTuple first argument has inappropriate value: "
+            ++ str1
+            )
+        )
+    | str1 == "udata"
+    = let midErr = makeUnitDataFromString str2
+      in  case midErr of
+              Left  err -> Left (SecondValueError (Left err))
+              Right mid -> fromTupleString (str1, str2)
---
+++ edition-helper/src/Primitive/Instance/UnitData.hs
@@ -0,0 +1,62 @@
+{-|
+Module : ModelId
+License : see LICENSE
+Description : UnitData primitive implements function definitions
+Copyright : Kaan Eraslan
+Maintainer : Kaan Eraslan
+Stability : Experimental
+-}
+
+module Primitive.Instance.UnitData where
+
+-- start def
+import           Primitive.Definition.UnitData  ( UnitData
+                                                    ( StringUnitDataCons
+                                                    , TextUnitDataCons
+                                                    )
+                                                )
+-- end def
+
+-- start fn
+import           FunctionDef.Setter             ( StringLike2Primitive
+                                                    ( fromString
+                                                    , fromText
+                                                    )
+                                                , TupleString2Primitive
+                                                    ( fromTupleString
+                                                    )
+                                                )
+import           FunctionDef.Transformer        ( Model2StringText
+                                                    ( toString
+                                                    , toText
+                                                    )
+                                                , Model2IdTuple(toIdTuple)
+                                                )
+-- end fn
+
+-- start utility
+import           Data.Text                      ( Text
+                                                , pack
+                                                , unpack
+                                                )
+-- end utility
+-- start setter
+
+instance StringLike2Primitive UnitData where
+    fromString astr = Right (StringUnitDataCons astr)
+
+instance TupleString2Primitive UnitData where
+    fromTupleString tpl = Right (StringUnitDataCons (snd tpl))
+
+-- end setter
+
+-- start transformer
+instance Model2StringText UnitData where
+    toString (StringUnitDataCons astr) = astr
+    toString (TextUnitDataCons   txt ) = unpack txt
+    toText (StringUnitDataCons astr) = pack astr
+    toText (TextUnitDataCons   txt ) = txt
+
+instance Model2IdTuple UnitData where
+    toIdTuple udata = ("udata", udata)
+-- end transformer
