558dd4f3f29606335458286fab405bbe5ca59b29
Kaan Eraslan <kaaneraslan@gmail.com>
2020-01-01 03:10:49 +0100

    fixed control.pure.modelattr errors

--- edition-helper/src/Control/Pure/ModelAttr.hs
+++ edition-helper/src/Control/Pure/ModelAttr.hs
@@ -26,9 +26,12 @@ import           FunctionDef.Setter             ( Map2Primitive(..)
 -- start utility
 import           Data.Map.Strict                ( elems
                                                 , keys
+                                                , toList
+                                                , filterWithKey
                                                 , Map
-                                                , filter
                                                 ) -- importing type
+import qualified Data.Map.Strict               as Mp
+                                                ( filter )
 import           Utils.StrUtils                 ( isAlphaNumStr
                                                 , isAsciiStr
                                                 )
@@ -36,24 +39,45 @@ import           Utils.MapUtils                 ( convertStringMap2Txt
                                                 , convertTxtMap2String
                                                 )
 
+showMapKeyVal :: Int -> Map String String -> String
+
+showMapKeyVal 0 amap = fst (head (toList amap))
+showMapKeyVal 1 amap = snd (head (toList amap))
+
+showMapKey :: Map String String -> String
+showMapKey = showMapKeyVal 0
+showMapVal :: Map String String -> String
+showMapVal = showMapKeyVal 1
+showFirstKV :: Map String String -> String
+showFirstKV amap =
+    "Key: " ++ (showMapKey amap) ++ "\n" ++ "Val: " ++ (showMapVal amap)
+
 -- end utility
 makeModelMapFromStringMap :: Map String String -> Either MapValueError ModelAttr
 makeModelMapFromStringMap aMap
-    | any null (elems aMap) = MapValError
-        "Attributes must have non empty values"
-        (filter any (null (elems aMap)))
-    | not (all isAlphaNumStr (elems aMap)) = MapValError
-        "Attributes must have alphanumeric values unlike: "
-        (filter isAlphaNumStr aMap)
-    | not (all isAsciiStr (elems aMap)) = MapValError
-        "Attributes must have ascii values"
-        (filter isAsciiStr aMap)
-    | not (all isAlphaNumStr (keys aMap)) = MapKeyError
-        "Attributes must have alphanumeric keys"
-        (filter isAlphaNumStr (keys aMap))
-    | not (all isAsciiStr (keys aMap)) = MapKeyError
-        "Attributes must have ascii keys"
-        (filter isAsciiStr (keys aMap))
-    | any null (keys aMap) = MapKeyError
-        "Attributes must have non empty keys"
-        (filter any (null (keys aMap)))
+    | any null (elems aMap)
+    = let errstr = showFirstKV (Mp.filter null aMap)
+      in  Left (MapValError "Attributes must have non empty values" errstr)
+    | any null (keys aMap)
+    = let errstr = showFirstKV (filterWithKey filtfn aMap)
+                  where filtfn key val = null key
+      in  Left (MapKeyError "Attributes must have non empty keys" errstr)
+    | not (all isAlphaNumStr (elems aMap))
+    = let errstr = showMapVal (Mp.filter isAlphaNumStr aMap)
+      in
+          Left
+              (MapValError
+                  "Attributes must have alphanumeric values unlike: "
+                  errstr
+              )
+    | not (all isAsciiStr (elems aMap))
+    = let errstr = showMapVal (Mp.filter isAsciiStr aMap)
+      in  Left (MapValError "Attributes must have ascii values" errstr)
+    | not (all isAlphaNumStr (keys aMap))
+    = let errstr = showMapKey (filterWithKey fn aMap)
+                  where fn key val = isAlphaNumStr key
+      in  Left (MapKeyError "Attributes must have alphanumeric keys" errstr)
+    | not (all isAsciiStr (keys aMap))
+    = let errstr = showMapKey (filterWithKey fn aMap)
+                  where fn key val = isAsciiStr key
+      in  Left (MapKeyError "Attributes must have ascii keys" errstr)
